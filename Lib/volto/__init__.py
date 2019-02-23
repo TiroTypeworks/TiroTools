a31a15308f5ef289b56d295f3bfea430a2fb3d38
Khaled Hosny <khaledhosny@eglug.org>
2019-02-23 18:36:57 +0200

    Minor

--- Lib/volto/__init__.py
+++ Lib/volto/__init__.py
@@ -475,28 +475,28 @@ class VoltToFea:
                 continue
             statement = None
             glyphs = self._coverage(key)
-            replacement = self._coverage(val)
+            replacements = self._coverage(val)
             if ignore:
                 chain_context = (prefix, glyphs, suffix)
                 statement = ast.IgnoreSubstStatement([chain_context])
             elif isinstance(sub, VoltAst.SubstitutionSingleDefinition):
                 assert(len(glyphs) == 1)
-                assert(len(replacement) == 1)
+                assert(len(replacements) == 1)
                 statement = ast.SingleSubstStatement(
-                    glyphs, replacement, prefix, suffix, chain)
+                    glyphs, replacements, prefix, suffix, chain)
             elif isinstance(sub, VoltAst.SubstitutionReverseChainingSingleDefinition):
                 assert(len(glyphs) == 1)
-                assert(len(replacement) == 1)
+                assert(len(replacements) == 1)
                 statement = ast.ReverseChainSingleSubstStatement(
-                    prefix, suffix, glyphs, replacement)
+                    prefix, suffix, glyphs, replacements)
             elif isinstance(sub, VoltAst.SubstitutionMultipleDefinition):
                 assert(len(glyphs) == 1)
                 statement = ast.MultipleSubstStatement(
-                    prefix, glyphs[0], suffix, replacement, chain)
+                    prefix, glyphs[0], suffix, replacements, chain)
             elif isinstance(sub, VoltAst.SubstitutionLigatureDefinition):
-                assert(len(replacement) == 1)
+                assert(len(replacements) == 1)
                 statement = ast.LigatureSubstStatement(
-                    prefix, glyphs, suffix, replacement[0], chain)
+                    prefix, glyphs, suffix, replacements[0], chain)
             else:
                 raise NotImplementedError(sub)
             statements.append(statement)
