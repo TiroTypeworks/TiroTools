8e4cfb97bf6450259472d68f16588c46c45f5111
Khaled Hosny <khaledhosny@eglug.org>
2019-05-13 11:47:53 +0200

    Release 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", encoding="utf-8") as readme:
 
 setup(
     name="Volto",
-    version="0.1.0",
+    version="0.1.1",
     description='MS VOLT .vtp to AFDKO .fea OpenType Layout converter',
     long_description=long_description,
     long_description_content_type='text/markdown',
