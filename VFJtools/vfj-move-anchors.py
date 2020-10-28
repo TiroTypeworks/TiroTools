9e1adf313bf3b378a721be1859b7ffc1fc8c973e
Khaled Hosny <khaledhosny@eglug.org>
2020-10-28 21:45:44 +0200

    [VFJtools] Some error handling in vfj-move-anchors.py

--- VFJtools/vfj-move-anchors.py
+++ VFJtools/vfj-move-anchors.py
@@ -2,9 +2,13 @@
 import argparse
 import csv
 import sys
+import logging
 
 from pathlib import Path
 from vfj import Font
+
+logging.basicConfig(format="%(levelname)s: %(message)s")
+log = logging.getLogger()
 
 
 def process(font, positions):
@@ -12,13 +16,20 @@ def process(font, positions):
     for name in positions:
         gname, x, y = positions[name]
         glyph = font[gname]
+        if glyph is None:
+            log.warning(f"Glyph '{gname}' is missing from font")
+            continue
+
         for layer in glyph.layers:
             if layer.name not in offsets:
                 offsets[layer.name] = {}
+            assert name[1:] not in offsets[layer.name]
             for anchor in layer.anchors:
                 if anchor.name == name:
                     xoff, yoff = x - anchor.x, y - anchor.y
                     offsets[layer.name][name[1:]] = (xoff, yoff)
+            if name[1:] not in offsets[layer.name]:
+                log.warning(f"Glyph '{gname}' does not have anchor named '{name}'")
 
     for glyph in font:
         for layer in glyph.layers:
@@ -52,9 +63,22 @@ def main(args=None):
     options = parser.parse_args(args)
 
     font = Font(options.input)
+
+    positions = {}
     with open(options.positions) as f:
         reader = csv.reader(f)
-        positions = {row[0]: (row[1], int(row[2]), int(row[3])) for row in reader}
+        for row in reader:
+            if len(row) != 4:
+                log.error("Invalid CSV data, there must exactly 4 columns.")
+                return 1
+
+            if row[0] in positions:
+                log.warning(
+                    f"Anchor '{row[0]}' is already set for another glyph, ignoring."
+                )
+                continue
+
+            positions[row[0]] = (row[1], int(row[2]), int(row[3]))
 
     process(font, positions)
 
